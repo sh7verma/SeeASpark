162ccacc5fa50d827cd52f4bf932b7becfbaafe7
Rajat <rajatarora@applify.co>
2018-05-24 14:42:16 +0530

    dev branch code

---
+++ app/src/main/java/com/seeaspark/WalkthroughActivity.kt
@@ -0,0 +1,21 @@
+package com.seeaspark
+
+import android.content.Context
+
+class WalkthroughActivity : BaseActivity() {
+    override fun initUI() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onCreateStuff() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun initListener() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getContentView() = R.layout.activity_walkthrough
+
+    override fun getContext() = this
+}
